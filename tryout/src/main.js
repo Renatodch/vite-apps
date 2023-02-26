25ff02a79a54826893cceaaeff02da62a35739aa
renatodch <renato.delcas@gmail.com>
2023-02-25 19:39:17 -0500

    sidebar

--- tryout/src/main.js
+++ tryout/src/main.js
@@ -4,10 +4,36 @@ import '@fortawesome/fontawesome-free/css/fontawesome.css';
 import '@fortawesome/fontawesome-free/css/solid.css';
 import '@fortawesome/fontawesome-free/css/brands.css';
 
-document.querySelector('.btn-main')?.addEventListener('click',()=>{
-  const menu = document.querySelector('.sidebar-menu');
-  if(menu){
-    menu.style.left='0%';
+document.querySelector('.switch')?.addEventListener('input',(e)=>{
+  const menu = document.querySelector('.menu');
+  const menuNavTitle = document.querySelector('.menu__nav-title');
+  const items = document.querySelectorAll('.menu__nav-item');
+  const menuNavList = document.querySelector('.menu__nav-list');
+
+  if(menu && menuNavTitle && menuNavList){    
+    menu.classList.toggle('menu_toggle-on')
+    menuNavTitle.classList.toggle('menu__nav-title_toggle-on')
+    menuNavTitle.innerHTML= menuNavTitle.classList.contains('menu__nav-title_toggle-on')?"APPS & PAGES":""
+
+    items.forEach(e=>{
+      e.classList.toggle('toggle-on')
+    })
+
+    menuNavList.classList.toggle('menu__nav-list_toggle-on')
+
+   /*  .menu__nav-item:nth-child(#{$key}) {
+      .menu__nav-link:after{
+          content: "#{$value}";
+          font-weight: bold;
+      }
+      &._toggle-on {
+          .menu__nav-link:after{
+              content: "";
+              font-weight: bold;
+          }
+      }
+  } */
+
   }
 });
 
